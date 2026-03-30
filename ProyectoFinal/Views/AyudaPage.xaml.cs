d4bbf3884951f0c88cd5606e19cb58d7ac4bc70a
Vflores17 <vidalfloresmontero@gmail.com>
2026-03-30 11:05:43 -0600

    Ayuda Basica
    Cambios en la pestaña ayuda basica

--- ProyectoFinal/Views/AyudaPage.xaml.cs
+++ ProyectoFinal/Views/AyudaPage.xaml.cs
@@ -1,3 +1,7 @@
+using System;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.ApplicationModel;
+
 namespace ProyectoFinal.Views;
 
 public partial class AyudaPage : ContentPage
@@ -11,4 +15,23 @@ public partial class AyudaPage : ContentPage
     {
         await Navigation.PopAsync();
     }
-}
+    
+
+    private async void OnContactarClicked(object sender, EventArgs e)
+    {
+        try
+        {
+            var message = new EmailMessage
+            {
+                Subject = "Soporte - Pura Vida Yachts",
+                Body = "Hola, necesito ayuda con la aplicación...",
+                To = new List<string> { "soporte@puravidayachts.com" }
+            };
+
+            await Email.Default.ComposeAsync(message);
+        }
+        catch (Exception)
+        {
+            await DisplayAlert("Error", "No se pudo abrir el correo.", "OK");
+        }
+    }
